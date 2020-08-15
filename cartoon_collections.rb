c090d76a986a4c6e9c3ae5d012944d3075cbea35
Payton Gray <paytongray18@gmail.com>
2020-08-15 17:06:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+  total = 0 
+  array.each do |dwarves_array|
+    puts #{dwarves_array.capitilize}
+    total += 1
+  end
 end
