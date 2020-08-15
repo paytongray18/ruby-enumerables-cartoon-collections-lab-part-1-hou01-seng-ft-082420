fe8d360cf85935fbf3ecfd7c0815d9c4de1423e7
Payton Gray <paytongray18@gmail.com>
2020-08-15 17:16:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def list_dwarves(array)
   total = 0 
-  array.each do |dwarves_array,index|
+  array.each_with_index do |dwarves_array,index|
     puts "#{index+=1}.#{dwarves_array.capitalize}"
     total += 1
   end
